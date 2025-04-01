200c7bbd8506c95cb237416436a38ebc891bddd7
Pedro Galveias <pedro.r.galveias@pm.me>
2025-04-01 18:11:06 +0100

    Polishing/Clean Up - Choosing a Random Port
    Tests should run their background application on a random available port

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,9 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check() {
     // No .await, no expect
-    spawn_app();
+    let address = spawn_app();
 
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application
@@ -9,7 +11,7 @@ async fn health_check() {
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -20,11 +22,18 @@ async fn health_check() {
 }
 
 // Launch the application in the background
-fn spawn_app() {
-    let server = farms::run().expect("Failed to bind address.");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+
+    let server = farms::run(listener).expect("Failed to bind address.");
 
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
