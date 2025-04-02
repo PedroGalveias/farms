30b4344cd435e010ec67548c9ea3a8908bd0ed31
Pedro Galveias <pedro.r.galveias@pm.me>
2025-04-02 13:43:31 +0100

    Format code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::net::TcpListener;
 use farms::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    
+
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on Server
     run(listener)?.await
