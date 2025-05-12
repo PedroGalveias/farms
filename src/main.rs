88f82b8624f869fcd4cfedb7e2ff97e9cfbf145d
Pedro Galveias <pedro.r.galveias@pm.me>
2025-05-12 16:07:31 +0200

    Add Log Logic & Error Fixing
    => Fix `run` function signature in `startup.rs`

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 //! src/main.rs
+use env_logger::Env;
 use farms::configuration::get_configuration;
 use farms::startup::run;
 use sqlx::{Connection, PgPool};
@@ -6,6 +7,11 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,18 @@
 use crate::routes::{create, health_check};
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
-use sqlx::PgConnection;
+use sqlx::PgPool;
 use std::net::TcpListener;
-pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // Wrap the connection in a smart pointer
-    let db_pool = web::Data::new(connection);
+    let db_pool = web::Data::new(db_pool);
     // Capture the `connection` from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
+            // Middlewares are added using the `wrap` method on `App`
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/farms", web::post().to(create))
             // Get pointer copy and attach it to the application state
