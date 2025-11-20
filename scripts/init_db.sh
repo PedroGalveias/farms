9ed42657f2a2d489b86d8de22ea814abfb12370b
Pedro Moleiro <pedro.r.p.moleiro@outlook.com>
2025-11-20 00:27:34 -0100

    Update CI/CD workflows

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,59 +2,66 @@
 set -x
 set -eo pipefail
 
-if ! [ -x "$(command -v psql)" ]; then
-  echo >&2 "Error: psql is not installed."
-  exit 1
-fi
-
 if ! [ -x "$(command -v sqlx)" ]; then
   echo >&2 "Error: sqlx is not installed."
   echo >&2 "Use:"
-  echo >&2 "    cargo install --version=0.8.3 sqlx-cli --no-default-features --features postgres"
+  echo >&2 "    cargo install --version=0.8.6 sqlx-cli --no-default-features --features postgres"
   echo >&2 "to install it."
   exit 1
 fi
 
 # Check if a custom user has been set, otherwise default to 'postgres'
 DB_USER="${POSTGRES_USER:=postgres}"
-
 # Check if a custom password has been set, otherwise default to 'password'
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-
 # Check if a custom database name has been set, otherwise default to 'newsletter'
 DB_NAME="${POSTGRES_DB:=farms}"
-
 # Check if a custom port has been set, otherwise default to '5432'
 DB_PORT="${POSTGRES_PORT:=5432}"
-
 # Check if a custom host has been set, otherwise default to 'localhost'
 DB_HOST="${POSTGRES_HOST:=127.0.0.1}"
 
 # Allow to skip Docker if a dockerized Postgres database is already running
 if [[ -z "${SKIP_DOCKER}" ]]
 then
+  RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
+  if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
+    echo >&2 "there is a postgres container already running, kill it with"
+    echo >&2 "    docker kill ${RUNNING_POSTGRES_CONTAINER}"
+    exit 1
+  fi
+  CONTAINER_NAME="postgres_$(date '+%s')"
   # Launch postgres using Docker
   docker run \
-    -e POSTGRES_USER="${DB_USER}" \
-    -e POSTGRES_PASSWORD="${DB_PASSWORD}" \
-    -e POSTGRES_DB="${DB_NAME}" \
-    -p "${DB_PORT}":5432 \
-    -d postgres \
-    postgres -N 1000
-    # ^ Increased maximum number of connections for testing purposes
-fi
+    --env POSTGRES_USER="${DB_USER}" \
+    --env POSTGRES_PASSWORD="${DB_PASSWORD}" \
+    --env POSTGRES_DB="${DB_NAME}" \
+    --health-cmd="pg_isready -U ${DB_USER} || exit 1" \
+    --health-interval=1s \
+    --health-timeout=5s \
+    --health-retries=5 \
+    --publish "${DB_PORT}":5432 \
+    --detach \
+    --name "${CONTAINER_NAME}" \
+    postgres:alpine -N 1000
+    #                ^ Increased maximum number of connections for testing purposes
 
-# Keep pinging Postgres until it's ready to accept commands
-export PGPASSWORD="${DB_PASSWORD}"
-until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
->&2 echo "Postgres is still unavailable - sleeping"
-sleep 1
-done
+    until [ \
+      "$(docker inspect -f "{{.State.Health.Status}}" "${CONTAINER_NAME}")" == \
+      "healthy" \
+    ]; do
+      >&2 echo "Postgres is still unavailable - sleeping"
+      sleep 1
+    done
+fi
 
 >&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
+
+# create the .env file
+#echo "DATABASE_URL=$DATABASE_URL" > .env
 
 sqlx database create
 sqlx migrate run
