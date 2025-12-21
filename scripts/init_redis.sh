42e7662402c64b130027b88b6b5cff7bb878ec0b
Pedro Moleiro <pedro.r.p.moleiro@outlook.com>
2025-12-21 13:06:28 -0100

    fix: use docker instead of podman on init script

--- scripts/init_redis.sh
+++ scripts/init_redis.sh
@@ -6,12 +6,12 @@ RUNNING_REDIS_CONTAINER=$(docker ps --filter 'name=valkey' --format '{{.ID}}')
 
 if [[ -n $RUNNING_REDIS_CONTAINER ]]; then
   echo >&2 "there is already a redis container running, kill it with:"
-  echo >&2 "\tpodman kill ${RUNNING_REDIS_CONTAINER}"
+  echo >&2 "\tdocker kill ${RUNNING_REDIS_CONTAINER}"
   exit 1
 fi
 
 # Launch new valkey using docker
-podman run \
+docker run \
   --publish 6379:6379 \
   --detach \
   --name "valkey_$(date '+%s')" \
